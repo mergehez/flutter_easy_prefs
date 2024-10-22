86fd1a9e6fc487ef230cbd73f08d6fd02203c0be
mergehez <argesozdogan@gmail.com>
2024-10-22 20:07:07 +0200

    quickfix: const BaseClassDef

--- packages/easy_prefs/lib/utils/base_class_def.dart
+++ packages/easy_prefs/lib/utils/base_class_def.dart
@@ -4,5 +4,5 @@ class BaseClassDef {
   final String name;
   final bool hasChangeNotifier;
 
-  BaseClassDef({required this.name, required this.hasChangeNotifier});
+  const BaseClassDef({required this.name, required this.hasChangeNotifier});
 }
